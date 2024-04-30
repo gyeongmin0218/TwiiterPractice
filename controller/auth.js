4e9e6da858d273e34a50640061ae839596667789
김경민 <kkm7900@gmail.com>
2024-04-30 11:03:51 +0900

    회원가입, 로그인 라우터 추가

--- controller/auth.js
+++ controller/auth.js
@@ -1,21 +1,21 @@
 import * as authRepository from '../data/auth.js';
 
 
-
 export async function signup(req, res, next){
-    const {username, password, name, email} =req.body;
-    const auth = await authRepository.createUser(username, password, name, email);
+    const { username, password, name, email } = req.body;
+    const users = await authRepository.createUser(username, password, name, email);
     if(users){
-    res.status(201).json(auth);
-}
+        res.status(201).json(users);
+    }
 }
 
 export async function login(req, res, next){
-    const {username, password} = req.body;
+    const { username, password } = req.body;
     const user = await authRepository.login(username);
     if(user){
-        res.status(201).json(`${username} 로그인 완료`)
-    }else{
-        res.status(404).json({message : `${username}님 아이디 또는 비밀번호 확인하세요`});
+        res.status(201).json(`${username} 로그인 완료`);
+    }
+    else{
+        res.status(404).json({ message: `${username} 님 아이디 또는 비밀번호를 확인해주세요` });
     }
 }
--- router/auth.js
+++ router/auth.js
@@ -6,9 +6,10 @@ import { validate } from '../middleware/validator.js';
 const router = express.Router();
 
 const validateSignup = [
-    body('username').trim().isLength({min: 3}).withMessage('최소 3자 이상 입력'),
-    body('password').trim().isLength({min: 4}).withMessage('최소 4자 이상 입력'),
-    body('email').trim().isEmail().withMessage('이메일 형식 확인'), validate
+    body('username').trim().isLength({ min: 3 }).withMessage('최소 3자 이상 입력'),
+    body('password').trim().isLength({ min: 4 }).withMessage('최소 4자 이상 입력'),
+    body('email').trim().isEmail().withMessage('이메일 형식 확인'),
+    validate
 ]
 
 router.post('/signup', validateSignup, authController.signup);
