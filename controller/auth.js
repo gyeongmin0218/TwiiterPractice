8dedb3e038e6597254f92d46b0ed2ad8d4332ae8
김경민 <kkm7900@gmail.com>
2024-05-03 10:01:42 +0900

    보안설정 개선

--- controller/auth.js
+++ controller/auth.js
@@ -1,12 +1,11 @@
 import * as authRepository from '../data/auth.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { config } from '../config.js';
 
-const secretKey = "abcd1234%^&*";
-const jwtExpiresInDays = '2d';
-const bcryptSaltRounds = 10;
+
 function createJwtToken(id){
-    return jwt.sign({id}, secretKey, {expiresIn: jwtExpiresInDays});
+    return jwt.sign({id}, config.jwt.secretKey, {expiresIn: config.jwt.expiresInSec});
 };
 
 export async function signup(req, res, next){
@@ -15,7 +14,7 @@ export async function signup(req, res, next){
     if(found){
         return res.status(409).json({message:`${username}이 이미 있습니다`});
     }
-    const hashed = await bcrypt.hash(password, bcryptSaltRounds);
+    const hashed = await bcrypt.hash(password, config.bcrypt.saltRounds);
     const userId = await authRepository.createUser({username, hashed, name, email, url});
     const token = createJwtToken(userId);
     res.status(201).json({token, username});
